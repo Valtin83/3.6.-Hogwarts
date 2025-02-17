3e8c881ec8816b8f9d6996dc6494a8bd1ac14520
zhe-v <zhe-valentin@mail.ru>
2025-02-17 22:52:09 +0700

    3.6 Hogwarts

---
+++ Hogwarts-shool/src/main/java/com/example/Hogwarts/shool/HogwartsShoolApplication.java
@@ -0,0 +1,13 @@
+package com.example.Hogwarts.shool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HogwartsShoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HogwartsShoolApplication.class, args);
+	}
+
+}
